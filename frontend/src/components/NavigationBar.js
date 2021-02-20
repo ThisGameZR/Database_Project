09dbeb7f475791239b0f35d23ba6bb4a7f0fa31e
Crimehunters <Watcharapon_jomngam@cmu.ac.th>
2021-02-20 22:23:00 +0700

    - Code cleaning and tweaking - Fixed Navigationbar (more simpler and correct in position) - Added dynamic product search bar (waited to be implemented further)

--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import {Container, Image, Navbar, Nav, NavDropdown, DropdownButton, Dropdown} from 'react-bootstrap';
 import axios from "axios";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import shoppingCart from './Images/shoppingCart.png';
 import LoginForm from './LoginForm';
 import './CSS/Navigation.css';
@@ -9,21 +8,21 @@ import './CSS/Navigation.css';
 function NavigationBar(){
 
     return(
-        <Container fluid >
+        <Container fluid>
             {/* Header bar */}
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
                 <Navbar.Brand href="/">
-                    <Image src={shoppingCart} roundedCircle style={{width:"100px",height:"100px"}}/>
+                    <Image src={shoppingCart} roundedCircle style={{width:"80px",height:"80px"}}/>
                     PLASTIC SHOP
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav" style={{marginLeft:"50px"}}>
-                    <Nav mr="auto">
-                        <Nav.Link href="/product">PRODUCT</Nav.Link>
-                        <NavDropdown title="Recommend" id="responsive-nav-dropdown" variant="dark">
-                            <NavDropdown.Item href="#" style={{color:"dodgerblue", fontSize:"14px"}}>Kitchen Tools</NavDropdown.Item>
-                            <NavDropdown.Item href="#" style={{color:"dodgerblue", fontSize:"14px"}}>Everyday Life</NavDropdown.Item>
-                            <NavDropdown.Item href="#" style={{color:"dodgerblue", fontSize:"14px"}}>Kid Toys</NavDropdown.Item>
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav mr="auto" style={{marginRight: "auto"}}>
+                        <NavDropdown title="PRODUCT" id="responsive-nav-dropdown" variant="dark">
+                        <NavDropdown.Item href="/products" style={{color:"dodgerblue", fontSize:"14px"}}>All</NavDropdown.Item>
+                            <NavDropdown.Item href="/products/kitchenTools" style={{color:"dodgerblue", fontSize:"14px"}}>Kitchen Tools</NavDropdown.Item>
+                            <NavDropdown.Item href="/products/everydayTools" style={{color:"dodgerblue", fontSize:"14px"}}>Everyday Life</NavDropdown.Item>
+                            <NavDropdown.Item href="/products/kidToys" style={{color:"dodgerblue", fontSize:"14px"}}>Kid Toys</NavDropdown.Item>
                         </NavDropdown>
                         <NavDropdown title="Support" id="responsive-nav-dropdown" variant="dark">
                             <NavDropdown.Item href="#">
@@ -39,7 +38,6 @@ function NavigationBar(){
                             </NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
-                    <div style={{width:"400px"}}></div>
                     <LoginForm/>
                 </Navbar.Collapse>
             </Navbar>
