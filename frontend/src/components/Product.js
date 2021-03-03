957374f99060f85ec7847eced0a980627c859241
Crimehunters <Watcharapon_jomngam@cmu.ac.th>
2021-03-03 21:49:38 +0700

    Product search filter (only on product name)

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,31 +1,65 @@
 import React, {Component} from "react";
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 import axios from "axios";
-import { Container, Navbar, InputGroup, FormControl } from "react-bootstrap";
+import { Container, Navbar, InputGroup, FormControl, Card, Button, CardColumns} from "react-bootstrap";
 
 export default class Product extends Component {
     constructor(){
         super();
         this.state = {
             name: "",
-            price: "not yet gotten"
+            loading: false,
+            error: null,
+            products: []
         }
+
+        this.GetAllProducts();
     }
-    buttonClick = () => {
-        axios.get("/product").then(res => {
-           this.setState({
-               name: res.data[1].ProductName,
-               price: res.data[1].Price
-            });
-        });
+
+    GetAllProducts = () => {
+        axios.get('/products').then(res => {
+            this.setState({
+                products: res.data
+            })
+        })
     };
 
     ProductSearch = (e) => {
-        let keyword = e.target.value;
-        this.setState({
-            name: keyword
+        axios.get('/products').then(res => {
+            this.setState({
+                products: res.data
+            });
+
+            let keyword = e.target.value;
+            let regx = new RegExp(keyword, 'i')
+            let productspecified = [];
+            
+            this.state.products.map((item) => {
+                if (regx.test(item.ProductName))
+                {
+                    productspecified.push(item)
+                }
+            })
+    
+            this.setState({
+                name: keyword,
+                products: productspecified
+            })
         })
     };
+
+    ProductRender = () => {
+        return this.state.products.map((item) => {
+            return (
+                <Card key={item.ID}>
+                    <Card.Body>
+                        <Card.Title>{item.ProductName}</Card.Title>
+                        <h1>${item.Price}</h1>
+                        <Button varient="primary" id={item.ID}>Add to Cart</Button>
+                    </Card.Body>
+                </Card>
+            )
+        })
+    }
 
     render() {
         return (
@@ -40,14 +74,10 @@ export default class Product extends Component {
                     </InputGroup>
                 </Navbar>
                 <h1>{this.state.name}</h1>
-                <Router>
-                    <Switch>
-                        {/* <Route path="/products" exact component={AllProduct}/>
-                        <Route path="/products/kitchentools" exact component={KitchenProduct}/>
-                        <Route path="/products/everydaytools" exact component={DailyTools}/>
-                        <Route path="/products/kidtoys" exact component={KidToys}/> */}
-                    </Switch>
-                </Router>
+
+                <CardColumns>
+                    {this.ProductRender()}
+                </CardColumns>
             </Container>
         )
     }
